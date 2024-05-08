85a282d0dfbd337469af702deffb0b7f2caccdc2
mat <mat@DESKTOP-29J9MOH>
2024-05-08 14:33:17 +0200

    cos

--- Source.cpp
+++ Source.cpp
@@ -363,33 +363,6 @@ int main(int argc, char** argv)
 	uint32_t lp = 0;
 
 	int64_t SumRenderTime = 0;
-
-	PerlinNoiseGenerator generator;
-	float x1, y1;
-	int w, h, idx;
-	w = 100;
-	h = 100;
-	float s = 0.1f;
-
-	int size = w * h;
-	char* texArr = new char[size];
-	for (y1 = 0; y1 < h; y1++) {
-		for (x1 = 0; x1 < w; x1++) {
-			idx = y1 * w + x1;
-			texArr[idx] = (char)((generator.perlin2DConfigurable(x1 * s, y1 * s, 121143, 1.0f, 8, 2.0f, 0.5f) * 0.5f + 0.5f) * 255.0f);
-		}
-	}
-
-	//Texture2D newText = { texArr, w, h, GL_RED, GL_RGBA };
-	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	//newText.genMipmap();
-	////r.newModel(RENDER_MODEL_SQUARE1, Square, simpleProgram, 6, GL_TRIANGLES, newText, 50);
-	//r.BindActiveModel(RENDER_MODEL_SQUARE1);
-	//r.md->std_texture2d = newText;
-
 	while (true)
 	{
 		evLoopStart = clock();
